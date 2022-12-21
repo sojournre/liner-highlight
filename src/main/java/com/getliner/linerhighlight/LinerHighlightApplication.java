c722e812bcf3fb7b024ae7387494a758825415c9
sojournre <sojournre@gmail.com>
2022-12-21 15:25:57 +0900

    Initial commit

---
+++ src/main/java/com/getliner/linerhighlight/LinerHighlightApplication.java
@@ -0,0 +1,13 @@
+package com.getliner.linerhighlight;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LinerHighlightApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LinerHighlightApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/getliner/linerhighlight/LinerHighlightApplicationTests.java
@@ -0,0 +1,13 @@
+package com.getliner.linerhighlight;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LinerHighlightApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
